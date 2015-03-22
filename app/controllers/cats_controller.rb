ae3eef274ab3860e85df338214c8d9b8cefc58ed
N. Woods-MacDonald <woods.macdonald@gmail.com>
2015-03-21 22:35:34 -0700

    standardized view names

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,14 +1,14 @@
 class CatsController < ApplicationController
   def new
-    render 'cats/new_cat'
+    render 'cats/new'
   end
 
   def create
     Cat.create(name: params[:name], age: params[:age])
-    render 'cats/post_cat'
+    render 'cats/create'
   end
 
   def index
-    render 'cats/get_cats'
+    render 'cats/index'
   end
 end
