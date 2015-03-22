7b2011a9d18f8b51ac7c45ffb41d486436ead101
N. Woods-MacDonald <woods.macdonald@gmail.com>
2015-03-21 22:38:09 -0700

    switched to standard implicit view rendering

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,14 +1,11 @@
 class CatsController < ApplicationController
   def new
-    render 'cats/new'
   end
 
   def create
     Cat.create(name: params[:name], age: params[:age])
-    render 'cats/create'
   end
 
   def index
-    render 'cats/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get  '/cats/new' => 'cats#new'
-  post '/cats'     => 'cats#create'
-  get  '/cats'     => 'cats#index'
+  resources :cats, only: [:new, :create, :index]
 end
