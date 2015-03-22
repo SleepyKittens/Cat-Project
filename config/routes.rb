c7d1c1296cab7201ae6fd74e9074c769eb79e15e
N. Woods-MacDonald <woods.macdonald@gmail.com>
2015-03-21 22:33:00 -0700

    standardized routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get  '/cats/new' => 'cats#new_cat'
-  get  '/new_cat'  => 'cats#new_cat'
   post '/cats'     => 'cats#post_cat'
   get  '/cats'     => 'cats#get_cats'
 end
